8405c227391124cb711563798eaf83821431a714
MIKE <mikael.agata.sitepu@gmail.com>
2026-02-20 10:58:21 +0700

    add app

---
+++ app.py
@@ -0,0 +1,14 @@
+# from flask import Flask, request
+
+# app = Flask(__name__)
+
+# @app.route('/kirim', methods=['POST'])
+# def kirim():
+#     data = request.json
+#     print("Pesan masuk:", data['pesan'])
+#     return {"status": "berhasil"}
+
+# app.run(host='0.0.0.0', port=5000)
+
+
+# tahap pengembangan
